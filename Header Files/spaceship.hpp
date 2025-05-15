46ad8e9cb5545f7813f9a6a7581b2b5ba15a2d0d
kmt24021639 <24021639@vnu.edu.vn>
2025-05-15 14:39:26 +0700

    Update spaceship.hpp

--- Header Files/spaceship.hpp
+++ Header Files/spaceship.hpp
@@ -14,9 +14,17 @@ class Spaceship {
         Rectangle getRect();
         void Reset();
         std::vector<Laser> lasers;
+        float ShootTime;
+        int LaserSpeed;
+        bool PUblaster;
+        bool PUFflasher;
+        bool PUtripleshoot;
+        bool PUxtreme;
+        bool PUshield;
     
     private:
         Texture2D image;
         Vector2 position;
         double lastFireTime;
+        Sound LaserSound;
 };
